3604494fe7c527ecab1b450a568948529a236ba3
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-05-02 03:57:20 -0400

    Implement better logging

--- lua/autorun/server/cfc_restart_alerter.lua
+++ lua/autorun/server/cfc_restart_alerter.lua
@@ -1,17 +1,25 @@
 local endpoint
 
+local function printLog(log)
+    print("[CFC Restart Alerter] " .. log)
+end
+
 local function getEndpoint()
-    return file.Read("restart_alerter_endpoint.txt", "DATA")
+    local contents = file.Read("cfc/restart_alerter_endpoint.txt", "DATA")
+    return string.gsub("[\r\n]", "", contents)[1]
 end
 
 local function alertOfRestart()
-    http.Post( endpoint, {}, function( result ) end, function( failed ) end )
+    printLog("Issueing a restart request to '" .. endpoint .. "'")
+    http.Post( endpoint, {}, function( result ) end, function( failed ) print("[CFC Restart Alerter] Request failed! : " .. failed) end )
 end
 
 endpoint = getEndpoint()
+printLog("Read endpoint as: " .. endpoint)
 if endpoint == nil then
-    print("[CFC Restart Alerter] The restart endpoint hasn't been set! Addon cannot function.")
+    printLog("The restart endpoint hasn't been set! Addon cannot function.")
 else
     hook.Remove("OnGamemodeLoaded","CFC_RestartAlerter")
     hook.Add("OnGamemodeLoaded", "CFC_RestartAlerter", alertOfRestart)
+    printLog("The restart script has been enabled!")
 end
