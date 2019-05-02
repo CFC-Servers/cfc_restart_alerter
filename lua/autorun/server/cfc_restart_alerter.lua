25cbd2b355be31fdfe330d64e905cdd1060bb2d1
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-05-02 04:20:59 -0400

    Delay the alert

--- lua/autorun/server/cfc_restart_alerter.lua
+++ lua/autorun/server/cfc_restart_alerter.lua
@@ -12,8 +12,14 @@ local function getEndpoint()
 end
 
 local function alertOfRestart()
+    timer.Simple()
     printLog("Issueing a restart request to '" .. endpoint .. "'")
     http.Post( endpoint, {}, function( result ) end, function( failed ) print("[CFC Restart Alerter] Request failed! : " .. failed) end )
+end
+
+local function delayedAlert()
+    -- We need to have a timer becasue sometimes this addon loads before HTTP functions are available
+    timer.Simple(3, alertOfRestart)
 end
 
 endpoint = getEndpoint()
@@ -22,6 +28,6 @@ if endpoint == nil then
     printLog("The restart endpoint hasn't been set! Addon cannot function.")
 else
     hook.Remove("Initialize","CFC_RestartAlerter")
-    hook.Add("Initialize", "CFC_RestartAlerter", alertOfRestart)
+    hook.Add("Initialize", "CFC_RestartAlerter", delayedAlert)
     printLog("The restart script has been enabled!")
 end
