ae18afc20e3ac6b6c78e370692db6b0eb82d7796
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-10-06 17:22:46 -0400

    Various spacing fixes

--- lua/autorun/server/cfc_restart_alerter.lua
+++ lua/autorun/server/cfc_restart_alerter.lua
@@ -1,41 +1,41 @@
 local endpoint
 
-local function printLog(log)
-    print("[CFC Restart Alerter] " .. log)
+local function printLog( log )
+    print( "[CFC Restart Alerter] " .. log )
 end
 
 local function getEndpoint()
-    local contents = file.Read("cfc/restart_alerter_endpoint.txt", "DATA")
-    local endpoint = string.gsub(contents, "%s", "")
+    local contents = file.Read( "cfc/restart_alerter_endpoint.txt", "DATA" )
+    local endpoint = string.gsub( contents, "%s", "" )
 
     return endpoint
 end
 
 local function alertOfRestart()
-    printLog("Issuing a restart request to '" .. endpoint .. "'")
+    printLog( "Issuing a restart request to '" .. endpoint .. "'" )
     http.Post(
         endpoint,
         {},
         function( result )
-            printLog("Request succeeded! : " .. result)
+            printLog( "Request succeeded! : " .. result )
         end,
 
         function( failed )
-            printLog("Request failed! : " .. failed)
+            printLog( "Request failed! : " .. failed )
         end
     )
 
-    printLog("Removing RestartAlerter Tick hook")
-    hook.Remove("Tick","CFC_RestartAlerter")
+    printLog( "Removing RestartAlerter Tick hook" )
+    hook.Remove( "Tick","CFC_RestartAlerter" )
 end
 
 endpoint = getEndpoint()
-printLog("Read endpoint as: " .. endpoint)
+printLog( "Read endpoint as: " .. endpoint )
 if endpoint == nil then
-    printLog("The restart endpoint hasn't been set! Addon cannot function.")
+    printLog( "The restart endpoint hasn't been set! Addon cannot function." )
 else
-    hook.Remove("Tick","CFC_RestartAlerter")
-    hook.Add("Tick", "CFC_RestartAlerter", alertOfRestart)
+    hook.Remove( "Tick","CFC_RestartAlerter" )
+    hook.Add( "Tick", "CFC_RestartAlerter", alertOfRestart )
 
-    printLog("The restart script has been enabled!")
+    printLog( "The restart script has been enabled!" )
 end
