d81098c05eadfec7047a45f42c6899bb3261b960
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-08-31 12:38:31 -0400

    Simplify

--- lua/autorun/server/cfc_restart_alerter.lua
+++ lua/autorun/server/cfc_restart_alerter.lua
@@ -1,16 +1,8 @@
 require( "webhooker_interface" )
 local Webhooker = WebhookerInterface()
-local endpoint
 
 local function printLog( log )
     print( "[CFC Restart Alerter] " .. log )
-end
-
-local function getEndpoint()
-    local contents = file.Read( "cfc/restart_alerter_endpoint.txt", "DATA" )
-    local endpoint = string.gsub( contents, "%s", "" )
-
-    return endpoint
 end
 
 local function onSuccess( result )
@@ -22,19 +14,12 @@ local function onFailure( failure )
 end
 
 local function alertOfRestart()
-    Webhooker:send( endpoint, {}, onSuccess, onFailure )
+    Webhooker:send( "gamemode-loaded", {}, onSuccess, onFailure )
 
     printLog( "Removing RestartAlerter Tick hook" )
     hook.Remove( "Tick","CFC_RestartAlerter" )
 end
 
-endpoint = getEndpoint()
-printLog( "Read endpoint as: " .. endpoint )
-if endpoint == nil then
-    printLog( "The restart endpoint hasn't been set! Addon cannot function." )
-else
-    hook.Remove( "Tick","CFC_RestartAlerter" )
-    hook.Add( "Tick", "CFC_RestartAlerter", alertOfRestart )
+hook.Add( "Tick", "CFC_RestartAlerter", alertOfRestart )
 
-    printLog( "The restart script has been enabled!" )
-end
+printLog( "The restart script has been enabled!" )
